80da0c7581175bd730d44690aaeafe4f61c66b60
AveryanovaKatia <160598695+AveryanovaKatia@users.noreply.github.com>
2024-06-15 19:50:22 +0400

    ТЗ-10 Filmorate

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -13,7 +13,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
