4825181419668239bfeb485ce63adc5c7a403e1e
AveryanovaKatia <160598695+AveryanovaKatia@users.noreply.github.com>
2024-07-28 02:04:06 +0500

    filmorate with DB after review

--- src/main/java/ru/yandex/practicum/filmorate/service/impl/GenreServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/impl/GenreServiceImpl.java
@@ -11,7 +11,6 @@ import ru.yandex.practicum.filmorate.repository.GenreRepository;
 import ru.yandex.practicum.filmorate.service.GenreService;
 
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @Service
